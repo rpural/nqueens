e723a243b63311a0d28c7b5feb2bb72c08c27684
Robert Nix <nix@earthling.net>
2019-01-21 18:03:26 -0600

    Add documentation strings to the functions

--- queens.py
+++ queens.py
@@ -1,8 +1,18 @@
 #! /usr/bin/env python3
 
 class Board:
+    ''' class Board
+
+    Emulate a chess board, of varying size, with the size given at the time
+    of creation.
+    '''
 
     def __init__( self, size=8 ):
+        ''' __init__(size)
+
+        Initialize a board, given its size. Size will default to 8, the
+        size of a regulation chess board.
+        '''
         print("<init>")
         self._board = []
         self._size = size
@@ -14,6 +24,14 @@ class Board:
         print("<init> size=" + str(self._size))
 
     def check_position( self, x, y ):
+        ''' check_position(x, y)
+
+        Check a board position to be sure that it is available for placement, and
+        that no other queen threatens the location. Returns True if a queen can
+        safely be placed in the location, and False if the position is threatened,
+        if there is already a queen there, or if the location is an invalid board
+        position.
+        '''
         print("<check> x = {}, y = {} board(x,y) = {}".format(x, y, self._board[x][y]))
         if 0 <= x < self._size: 
             pass
@@ -36,6 +54,10 @@ class Board:
         return True
 
     def set_position( self, x, y ):
+        ''' set_position(x, y)
+        
+        Place a queen on the board at the given location.
+        '''
         print("<set> x = {}, y = {}, size = {}".format(x, y, self._size))
         if 0 <= x < self._size:
             pass
@@ -55,12 +77,20 @@ class Board:
             return False
 
     def clear_position( self, x, y ):
+        ''' clear_position(x, y)
+
+        Clear a queen from the given location
+        '''
         print("<clear> x = {}, y = {}, size = {}".format(x, y, self._size))
         if 0 <= x < self._size and 0 <= y < self._size:
             self._board[x][y] = 0
         return True
 
     def __str__( self ):
+        ''' __str__()
+
+        Create a printable string which represents the current state of the board.
+        '''
         result = "-" * ( self._size * 2 + 2 ) + "\n"
         for row in range( self._size ):
             result += "|"
@@ -74,6 +104,9 @@ class Board:
         return result
 
 if __name__ == "__main__":
+    ''' If run as a main program, test the features of Board.
+    '''
+
     b = Board( 8 )
 
     print(b)
