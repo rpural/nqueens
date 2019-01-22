df4edab563729311c83516944e97d6d20361c8d8
Robert Nix <nix@earthling.net>
2019-01-21 18:17:49 -0600

    Add some more documentation

--- queens.py
+++ queens.py
@@ -33,19 +33,22 @@ class Board:
         position.
         '''
         print("<check> x = {}, y = {} board(x,y) = {}".format(x, y, self._board[x][y]))
-        if 0 <= x < self._size: 
+
+        # Are x and y within range?
+        if 0 <= x < self._size and 0 <= y < self._size: 
             pass
         else:
             return False
-        if 0 <= y < self._size:
-            pass
-        else:
-            return False
+
+        # Is there a queen at the location?
         if self._board[x][y]  == 1:
             return False
+
+        # Is there a queen anywhere within the location's row?
         if 1 in self._board[x]:
             return False
 
+        # Is there a queen anywhere within the location's column?
         for row in range( self._size ):
             if not ( row == x ):
                 if self._board[row][y] == 1:
@@ -59,15 +62,12 @@ class Board:
         Place a queen on the board at the given location.
         '''
         print("<set> x = {}, y = {}, size = {}".format(x, y, self._size))
-        if 0 <= x < self._size:
+
+        # Are x and y within range?
+        if 0 <= x < self._size and 0 <= y < self._size:
             pass
         else:
-            print( "x out of range: {}".format(x))
-            return False
-        if 0 <= y < self._size:
-            pass
-        else:
-            print( "y out of range: {}".format(y))
+            print( "out of range: {}")
             return False
 
         if self._board[x][y] == 0:
@@ -82,6 +82,8 @@ class Board:
         Clear a queen from the given location
         '''
         print("<clear> x = {}, y = {}, size = {}".format(x, y, self._size))
+
+        # Are x and y within range?
         if 0 <= x < self._size and 0 <= y < self._size:
             self._board[x][y] = 0
         return True
@@ -91,48 +93,48 @@ class Board:
 
         Create a printable string which represents the current state of the board.
         '''
+
+        # Start by drawing a line above the board
         result = "-" * ( self._size * 2 + 2 ) + "\n"
         for row in range( self._size ):
-            result += "|"
+            result += "|"  # Print the left side of the board
             for col in range( self._size ):
                 if self._board[row][col] == 1:
-                    result += "Q "
+                    result += "Q "  # If a queen, print Q, else print .
                 else:
                     result += ". "
-            result += "|\n"
-        result += "-" * ( self._size * 2 + 2 ) + "\n"
-        return result
+            result += "|\n"  # Print the right side of the board
+        result += "-" * ( self._size * 2 + 2 ) + "\n"  # Print the board bottom
+        return result  # Return the printable board as a text string
 
 if __name__ == "__main__":
     ''' If run as a main program, test the features of Board.
     '''
 
-    b = Board( 8 )
+    b = Board( 8 )  # Create a chess board
 
-    print(b)
+    print(b)  # Print the empty board (should be all dots)
 
-    if b.check_position( 3, 3 ):
-        b.set_position( 3, 3 )
-    print( b.check_position( 3, 3 ))
-    print(b)
+    if b.check_position( 3, 3 ):  # Be sure (3,3) is empty (which it should be)
+        b.set_position( 3, 3 )    # and place a queen there
+    print( b.check_position( 3, 3 ))  # This should print "False"
+    print(b)  # Print the board with a queen at (3,3)
 
-    b.set_position( 4, 4 )
-    print(b)
+    b.set_position( 4, 4 )  # Place a queen at (4,4)
+    print(b)  #  Print the board
 
-    for i in range(b._size):
+    for i in range(b._size):  # Print a numeric representation of the board
         print( b._board[i] )
 
-    print(b)
+    b.clear_position( 3, 3 )  # Remove the queen at (3,3)
+    print(b)  # and print the board
 
-    b.clear_position( 3, 3 )
-    print(b)
+    b.set_position( 10, 10 )  # Try to place a queen out of bounds
+    b.set_position( 7, 8 )    # And one a bit closer, but still out of bounds
+    b.set_position( 6, -1 )   # And one out of bounds at the otehr end
+    print(b)  # Print the board (Should still be as above)
 
-    b.set_position( 10, 10 )
-    b.set_position( 7, 8 )
-    b.set_position( 6, -1 )
-    print(b)
-
-    print( b.check_position( 2, 4 ))
+    print( b.check_position( 2, 4 ))  # Check several positions on the board
     print( b.check_position( 4, 2 ))
     print( b.check_position( 4, 4 ))
     print( b.check_position( 5, 5 ))
