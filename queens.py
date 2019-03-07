970963f7e0b138167f56df18ad0065f14d52b9cb
Robert Nix <nix@earthling.net>
2019-03-07 16:03:55 -0600

    Add a second method of finding the solution, and time the two solutions

--- queens.py
+++ queens.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+from itertools import permutations
+
+from timer import Timer
 from board import Board
 
 def place_queen( board, row=0 ):
@@ -8,10 +11,30 @@ def place_queen( board, row=0 ):
         if board.check_position( row, i ):
             board.set_position( row, i )
             if board.count_queens() == board.get_size():
-                print( "Solution: {}\n".format( board.incr_solution_count()) + str( board ))
+                count = board.incr_solution_count()
+                # print( "Solution: {}\n".format( count ) + str( board ))
             else:
                 place_queen( board, row + 1 )
             board.clear_position( row, i )
+
+
+def permute_queens( boardsize ):
+
+    solutions = []
+
+    for p in permutations(list(range(boardsize))):
+        diag1 = set()
+        diag2 = set()
+
+        for i in range(boardsize):
+            diag1.add(p[i]+i)
+            diag2.add(p[i]-i)
+
+        if boardsize == len(diag1) == len(diag2):
+            solutions.append(p)
+
+    return len(solutions)
+
 
 
 size = int( input( "Enter the number of rows on the board: " ))
@@ -22,6 +45,16 @@ else:
     print("Could not allocate a board of size {}".format( size ))
     exit()
 
-place_queen( cb )
+with Timer() as t:
+    place_queen( cb )
 
 print( "Number of solutions: {}".format( cb.get_solution_count()))
+
+print("Solution took {:.3f} seconds".format(t.interval))
+
+with Timer() as t:
+    solution_count = permute_queens( size )
+
+print( "Number of solutions: {}".format( solution_count ))
+
+print("Solution took {:.3f} seconds".format(t.interval))
